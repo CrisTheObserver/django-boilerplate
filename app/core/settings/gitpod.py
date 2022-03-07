e366fd842ca3e22e0d9f5c6f7c963ae98acfb418
Juan Arriagada <jarriagada@dcc.uchile.cl>
2022-03-07 14:47:03 +0000

    blacked

--- app/core/settings/gitpod.py
+++ app/core/settings/gitpod.py
@@ -3,8 +3,8 @@ from core.settings.base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "GITPOD_HOST"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "8000-dccfcfmuchile-djangoboil-8jscpycf17e.ws-us34.gitpod.io"]
 
-CSRF_TRUSTED_ORIGINS = ["GITPOD_URL"]
+CSRF_TRUSTED_ORIGINS = ["https://8000-dccfcfmuchile-djangoboil-8jscpycf17e.ws-us34.gitpod.io"]
 
 ANALYTICS = ""
