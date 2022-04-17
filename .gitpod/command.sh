6fdeb3cde4b156ebf624887c3c61033927820437
Juan Arriagada <jarriagada@dcc.uchile.cl>
2022-04-17 22:59:23 +0000

    upd gitpod cfg

--- .gitpod/command.sh
+++ .gitpod/command.sh
@@ -2,14 +2,6 @@
 
 echo COMMAND
 
-cd /workspace/django-boilerplate/app
-pip install --upgrade pip
-pip install -r _requirements/base.txt -r _requirements/develop.txt
-pur -r _requirements/base.txt
-pur -r _requirements/production.txt
-
 source .gitpod/env.sh
 cd /workspace/django-boilerplate/app
-python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('desarrollo', '', 'desarroll0')" | python manage.py shell
 make run
--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -2,8 +2,6 @@
 
 echo INIT
 
-psql -U gitpod -c 'CREATE DATABASE boilerplate;'
-
 git submodule sync
 git submodule foreach git checkout main
 git submodule foreach git pull
@@ -12,3 +10,5 @@ source .gitpod/env.sh
 cd /workspace/django-boilerplate/app
 sed -i "s|GITPOD_HOST|8000-$GITPOD_HOST|g" core/settings/gitpod.py
 sed -i "s|GITPOD_URL|https://8000-$GITPOD_HOST|g" core/settings/gitpod.py
+python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('desarrollo', '', 'desarroll0')" | python manage.py shell
