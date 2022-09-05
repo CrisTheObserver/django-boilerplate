aef506eb682389ea32e99f9469c1d8b8391ced8f
Juan Arriagada <jarriagada@dcc.uchile.cl>
2022-09-05 16:10:48 +0000

    🚸 django admin/admin

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -33,4 +33,4 @@ psql -U gitpod -c "SELECT 1 FROM pg_database WHERE datname = 'boilerplate'" | gr
 
 cd /workspace/django-boilerplate/app
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('desarrollo', '', 'desarroll0')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
